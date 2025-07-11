cb16824dae122f80cc3c66c2d7a1efcfde42d4a9
Sumit Khandelwal <synergy2411@outlook.com>
2025-07-11 12:38:00 +0530

    custom validation added

--- frontend/src/app/components/auth/register/register.component.ts
+++ frontend/src/app/components/auth/register/register.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,13 +18,43 @@ export class RegisterComponent {
 
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
-      username: new FormControl(),
-      password: new FormControl(),
+      username: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        RegisterComponent.exclamationValidator,
+      ]),
+      cnfPassword: new FormControl(),
     });
+  }
+
+  get username() {
+    return this.registerForm.get('username') as FormControl;
+  }
+
+  get password() {
+    return this.registerForm.get('password') as FormControl;
+  }
+
+  get cnfPassword() {
+    return this.registerForm.get('cnfPassword') as FormControl;
   }
 
   onSubmit() {
     console.log('Username : ', this.registerForm.value.username);
     console.log('Password : ', this.registerForm.value.password);
+  }
+
+  // Custom Validator Function for checking Exclamation Sign
+  static exclamationValidator(
+    control: AbstractControl
+  ): ValidationErrors | null {
+    console.log(control);
+    const hasExclamation = control.value.indexOf('!');
+    if (hasExclamation >= 0) {
+      return null;
+    } else {
+      return { exclamation: true };
+    }
   }
 }
